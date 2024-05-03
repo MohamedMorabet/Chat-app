f1cccf311ad53ab3660a9b9a60d7eaba86fc6e65
MohamedMorabet <medmrbt5@email.com>
2024-05-03 13:08:01 +0100

    SignUp Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,21 +1,58 @@
 import User from "../models/user.modul.js";
+import bcrypt from "bcryptjs";
+
+
 
 export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body;
 
         if (password !== confirmPassword) {
-            return res.status(400).json({ message: "Passwords do not match" });
+            return res.status(400).json({ error: "Passwords do not match" });
         }
 
         const user = await User.findOne({ username });
-        if (user) {
-            return res.status(400).json({ message: "Username already exists" });
-        }
         
-        //Hash password
-    } catch (error) {
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
 
+        //Hash password
+        const salt = await bcrypt.genSalt(10); // 10 rounds of hashing 
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //Generate profile pic
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        //Create new user
+        const newUser = new User({
+            fullName,
+            username,
+            password : hashedPassword,
+            gender,
+            profilePic: gender === "Male" ? boyProfilePic : girlProfilePic
+        });
+
+        if (newUser) {
+            //Generate JWT token here
+            
+            await newUser.save();
+
+            //Send response
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.log("Error in sign up controller", error.message);
+        res.status(500).json({ error : "Internal Server Error" });
     }
 }
 
