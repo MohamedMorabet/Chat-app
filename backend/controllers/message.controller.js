506ab1bd5ec495a7f56cde81e1a7452ee0d62ac5
MohamedMorabet <medmrbt5@email.com>
2024-05-03 23:35:50 +0100

    SendMessage Endpoint

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,9 @@
+const sendMessage = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export default sendMessage;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,6 +12,7 @@ dotenv.config();
 app.use(express.json()); // for parsing application/json
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 app.listen(PORT, () => {
